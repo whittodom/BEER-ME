495c0db7ef9cd93805d97c09f1a3b7e33ceccf42
whittaker odom <Whitt@whittakers-MacBook-Pro.local>
2017-09-18 11:55:55 -0500

    HTML skeleton

---
+++ javascript/app.js
@@ -0,0 +1,16 @@
+
+$( document ).ready(function(){
+
+	//Materialize JS
+	$(".button-collapse").sideNav(); //initialize mobile format
+
+	$('select').material_select(); //initialize multiple selection drop-down menu
+
+
+
+
+
+
+
+
+});
